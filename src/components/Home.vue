1e9e8e071529de6064e9927978510fcd1780b8b9
mike_tung <miketung2013@gmail.com>
2018-01-23 22:31:48 -0500

    project ready to start

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,13 @@
 <template>
-    <div class="home-page">
-    </div>
+  <div class="home-page">
+    <h1>Welcome to Cat Lovers!</h1>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "home"
-    }
+export default {
+  name: 'home'
+};
 </script>
 
 <style scoped>
